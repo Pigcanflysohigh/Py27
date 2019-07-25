c83f8785a52d1c8ab9f0419e08c0c5fd3dfc1ddd
Pigcanflysohigh <729167925@qq.com>
2019-07-25 22:35:50 +0800

    0725_01

--- classes/day03/homework/homework05.py
+++ classes/day03/homework/homework05.py
@@ -5,14 +5,14 @@ lst_ipt_fir = []
 with open('stock_data.txt',mode='r',encoding='utf-8') as f:
     a = 0
     for line in f:
-        line_new = line.strip().replace('\t','|')   #获得字符串
+        line_new = line.strip()   #获得字符串
         #print(line_new)
         lst_data = line_new.split('\t')     #获得列表
-        dic_data[a] = line_new      #获得字典(元素是string)
+        dic_data[a] = lst_data      #获得字典(元素是list)
         a += 1
         #print(lst_data)
     print(dic_data)
-    ipt = input('输入你的筛选条件')
+    ipt = input('股票查询接口>>>')
     for i in ipt:
         lst_ipt_fir.append(i)
     #print(lst_ipt_fir)
@@ -27,7 +27,8 @@ with open('stock_data.txt',mode='r',encoding='utf-8') as f:
         #print('不带大于小于号')
         #print(dic_data[0])
         for k in dic_data:
-            if ipt in dic_data[k]:
-                print(dic_data[k])
+            for i in dic_data[k]:
+                if ipt in i:
+                    print(dic_data[k])
 
 #如何将表头和下面数据分开，在条件匹配的时候不需要去匹配表头
--- classes/day03/homework/test.py
+++ classes/day03/homework/test.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+'''
 list = ['a','b','c','d','e','f']
 dic = {}
 a = 0
@@ -16,3 +17,18 @@ for k in dic:
 
 dic.pop(0)
 print(dic)
+
+'''
+dic_data = {0:['序号', '代码', '名称', '相关链接', '最新价', '涨跌幅', '涨跌额', '成交量(手)', '成交额', '振幅', '最高', '最低', '今开', '昨收', '量比', '换手率', '市盈率(动态)', '市净率']}
+
+
+ipt = input('>>>')
+def find_index(args):
+    for i in dic_data[0]:
+        if i == args:
+            index_ipt = dic_data[0].index(i)
+            #print(index_ipt)
+            return index_ipt
+ret = find_index(ipt)
+print(ret)
+
